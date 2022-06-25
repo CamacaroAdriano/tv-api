cef5b011217928853575f160daf928a8be5680c7
acamacaro <acamacaro@octopuslab.it>
2022-06-25 18:04:49 +0200

    feat(tv-maze-client): Added yet to implement TVMazeClient@searchShowsByName method test

---
+++ tests/Clients/TVMazeClient/SearchShowsByNameTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Clients\TVMazeClient;
+
+use Illuminate\Support\Facades\Http;
+use Src\Clients\TVMazeClient;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class SearchShowsByNameTest extends TestCase
+{
+    public function dataProvider(): array
+    {
+        return [
+            [['show-1', 'show-2', 'show-3'], Response::HTTP_OK],
+            [null, Response::HTTP_BAD_REQUEST]
+        ];
+    }
+
+    /**
+     * @param array|null $responseBody
+     * @param int $status
+     * @return void
+     * @dataProvider dataProvider
+     */
+    public function testSearchShowsByNameMethod(?array $responseBody, int $status): void
+    {
+        Http::fake(['*' => Http::response(['show-1', 'show-2', 'show-3'], $status)]);
+
+        $this->assertEquals($responseBody, (new TVMazeClient())->searchShowsByName('show'));
+    }
+}
