85f5e32771daf9e55f754e4d5af8568b001663f7
acamacaro <acamacaro@octopuslab.it>
2022-06-25 20:36:23 +0200

    feat(tv-maze-client): Updated TVMazeClient@searchShowsByName test
     Method should always return Illuminate\Support\Collection instance
     Method should throw Exception with 502 status code if external API calls respond with >=500 status code

--- tests/Clients/TVMazeClient/SearchShowsByNameTest.php
+++ tests/Clients/TVMazeClient/SearchShowsByNameTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Clients\TVMazeClient;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
 use Src\Clients\TVMazeClient;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,20 +15,31 @@ class SearchShowsByNameTest extends TestCase
         return [
             [['show-1', 'show-2', 'show-3'], Response::HTTP_OK],
             [null, Response::HTTP_BAD_REQUEST],
-            [null, Response::HTTP_INTERNAL_SERVER_ERROR],
+            [null, Response::HTTP_FOUND]
         ];
     }
 
     /**
-     * @param array|null $responseBody
+     * @param ?array $expectedReturnValue
      * @param int $status
      * @return void
      * @dataProvider dataProvider
+     * @throws Exception
      */
-    public function testSearchShowsByNameMethod(?array $responseBody, int $status): void
+    public function testSearchShowsByNameMethod(?array $expectedReturnValue, int $status): void
     {
         Http::fake(['*' => Http::response(['show-1', 'show-2', 'show-3'], $status)]);
 
-        $this->assertEquals(collect($responseBody), (new TVMazeClient())->searchShowsByName('show'));
+        $this->assertEquals(collect($expectedReturnValue), (new TVMazeClient())->searchShowsByName('show'));
+    }
+
+    public function testSearchShowsByNameShouldThrowException(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(Response::HTTP_BAD_GATEWAY);
+
+        Http::fake(['*' => Http::response([], Response::HTTP_INTERNAL_SERVER_ERROR)]);
+
+        (new TVMazeClient())->searchShowsByName('show');
     }
 }
