9f10307a485d847087a0f11317fa47f13b65006b
acamacaro <acamacaro@octopuslab.it>
2022-06-26 19:23:13 +0200

    feat(caching-tv-maze-client-responses): Updated TVMazeClient@searchShowsByName method test to implement caching of responses from TV Maze API.

--- tests/Clients/TVMazeClient/SearchShowsByNameTest.php
+++ tests/Clients/TVMazeClient/SearchShowsByNameTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Clients\TVMazeClient;
 
 use Exception;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Src\Clients\TVMazeClient;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,8 +31,11 @@ class SearchShowsByNameTest extends TestCase
     public function testSearchShowsByNameMethod(?array $expectedReturnValue, int $status): void
     {
         Http::fake(['*' => Http::response(['show-1', 'show-2', 'show-3'], $status)]);
-
         $this->assertEquals(collect($expectedReturnValue), (new TVMazeClient())->searchShowsByName('show'));
+
+        Http::assertSent(function (Request $request) {
+            return $request->url() === env('TV_MAZE_API_URL') . TVMazeClient::SEARCH_SHOW_PATH . '?q=show';
+        });
     }
 
     public function testSearchShowsByNameShouldThrowException(): void
@@ -41,5 +46,40 @@ class SearchShowsByNameTest extends TestCase
         Http::fake(['*' => Http::response([], Response::HTTP_INTERNAL_SERVER_ERROR)]);
 
         (new TVMazeClient())->searchShowsByName('show');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testSearchShowsByNameShouldReturnCachedValue(): void
+    {
+        $showName = 'show';
+        $cachedResult = collect();
+
+        Http::fake();
+        Cache::shouldReceive('has')->once()->with($showName)->andReturnTrue();
+        Cache::shouldReceive('get')->once()->with($showName)->andReturn($cachedResult);
+
+        $this->assertEquals($cachedResult, (new TVMazeClient())->searchShowsByName($showName));
+
+        Http::assertNothingSent();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testSearchShowsByNameShouldCallExternalApiAndCacheResult(): void
+    {
+        $showName = 'show';
+
+        Http::fake();
+        Cache::shouldReceive('has')->once()->with($showName)->andReturnFalse();
+        Cache::shouldReceive('put');
+
+        (new TVMazeClient())->searchShowsByName($showName);
+
+        Http::assertSent(function (Request $request) use ($showName) {
+            return $request->url() === env('TV_MAZE_API_URL') . TVMazeClient::SEARCH_SHOW_PATH . '?q=' . $showName;
+        });
     }
 }
